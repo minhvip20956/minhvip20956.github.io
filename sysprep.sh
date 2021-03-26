7f9e026d9ebb84fb35217943d22cac097f82052c
Cong Minh Vuong <cminh.v566@gmail.com>
2021-03-26 15:32:29 +0700

    Update sysprep.sh

--- sysprep.sh
+++ sysprep.sh
@@ -1,19 +1,31 @@
-# SySPrep only for CentOS
-# By Cong Minh
-# Run with command
-# curl -o sysprep.sh https://raw.githubusercontent.com/minhvip20956/minhvip20956.github.io/master/sysprep.sh && chmod +x sysprep.sh && sh sysprep.sh
-#
-echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6=1" >> /etc/sysctl.conf
+## SySPrep only for CentOS
+## By Cong Minh
+## Run with command
+## curl -o sysprep.sh https://raw.githubusercontent.com/minhvip20956/minhvip20956.github.io/master/sysprep.sh && chmod +x sysprep.sh && sh sysprep.sh
+##
+
+# Disabled Firewalld
 systemctl disable firewalld
+
+# Disabled SELINUX
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+# Update OS
 yum update --skip-broken -y
+yum upgrade -y
+yum autoremove -y
+
+# Install Default Software and QEMU Service
 yum install nano screen qemu-guest-agent -y
 systemctl enable qemu-guest-agent
+
+# Install Yum Cron
 yum install yum-cron -y
 sed -i 's/download_updates = no/download_updates = yes/g' /etc/yum/yum-cron.conf
 sed -i 's/apply_updates = no/apply_updates = yes/g' /etc/yum/yum-cron.conf
 systemctl enable yum-cron
+
+# Clean up System
 /sbin/service rsyslog stop
 /sbin/service auditd stop
 /bin/package-cleanup --oldkernels --count=1
@@ -37,5 +49,7 @@ unset HISTFILE
 /bin/rm -rf ~root/.ssh/
 /bin/rm -f ~root/anaconda-ks.cfg
 /bin/rm -f sysprep.sh
+
+# System go to SysPrep Mode
 history -c
 sys-unconfig
